4e81935697b0d7664e7e32787cdd32423ab75263
Pedro Lucas <plfm3k@gmail.com>
2026-04-11 22:35:05 -0400

    feat: adiciona entidade Category

---
+++ backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/entities/Category.java
@@ -0,0 +1,29 @@
+package dev.pedro.foodflow_api.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+}
