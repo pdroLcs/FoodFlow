2eb309050dd74e7d56d5d65264bdbc4dde767f5b
Pedro Lucas <plfm3k@gmail.com>
2026-04-12 19:20:18 -0400

    feat: adiciona ProductMapper

---
+++ backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/mappers/ProductMapper.java
@@ -0,0 +1,12 @@
+package dev.pedro.foodflow_api.mappers;
+
+import dev.pedro.foodflow_api.dto.ProductResponseDTO;
+import dev.pedro.foodflow_api.entities.Product;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", uses = CategoryMapper.class)
+public interface ProductMapper {
+
+    ProductResponseDTO toDTO(Product product);
+
+}
