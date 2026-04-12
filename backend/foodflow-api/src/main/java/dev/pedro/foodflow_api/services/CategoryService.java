ffd134e4a35e882a720f3f231ec7283851883a0e
Pedro Lucas <plfm3k@gmail.com>
2026-04-12 14:39:27 -0400

    feat: adiciona CategoryService

---
+++ backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/services/CategoryService.java
@@ -0,0 +1,31 @@
+package dev.pedro.foodflow_api.services;
+
+import dev.pedro.foodflow_api.dto.CategoryResponseDTO;
+import dev.pedro.foodflow_api.entities.Category;
+import dev.pedro.foodflow_api.mappers.CategoryMapper;
+import dev.pedro.foodflow_api.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    public CategoryService(CategoryRepository categoryRepository, CategoryMapper categoryMapper) {
+        this.categoryRepository = categoryRepository;
+        this.categoryMapper = categoryMapper;
+    }
+
+    public CategoryResponseDTO createCategory(String name) {
+        var category = Category.builder().name(name).build();
+        var savedCategory = categoryRepository.save(category);
+        return categoryMapper.toDTO(savedCategory);
+    }
+
+    public List<CategoryResponseDTO> listCategories() {
+        return categoryRepository.findAll().stream().map(categoryMapper::toDTO).toList();
+    }
+}
