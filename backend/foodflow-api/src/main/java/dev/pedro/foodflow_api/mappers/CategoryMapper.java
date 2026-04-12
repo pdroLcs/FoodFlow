7227b8c5378bbf5d9b08e7e6808c8e5d5a43679d
Pedro Lucas <plfm3k@gmail.com>
2026-04-12 14:33:24 -0400

    feat: adiciona mapper CategoryMapper

---
+++ backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/mappers/CategoryMapper.java
@@ -0,0 +1,14 @@
+package dev.pedro.foodflow_api.mappers;
+
+import dev.pedro.foodflow_api.dto.CategoryResponseDTO;
+import dev.pedro.foodflow_api.entities.Category;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+
+    CategoryResponseDTO toDTO(Category category);
+
+    Category toEntity(String name);
+
+}
