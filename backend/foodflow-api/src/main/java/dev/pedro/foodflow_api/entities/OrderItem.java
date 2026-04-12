d0c96936abe95237f1a457bc997f32a94280b797
Pedro Lucas <plfm3k@gmail.com>
2026-04-12 11:35:35 -0400

    feat: adiciona setters na entidade OrderItem

--- backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/entities/OrderItem.java
+++ backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/entities/OrderItem.java
@@ -1,10 +1,7 @@
 package dev.pedro.foodflow_api.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,6 +10,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 @Builder
 public class OrderItem {
 
