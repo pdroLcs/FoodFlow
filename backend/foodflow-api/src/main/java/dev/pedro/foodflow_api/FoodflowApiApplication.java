d96257b714b0c79b1942bcf5a9a083780b14fbd9
Pedro Lucas <plfm3k@gmail.com>
2026-04-11 21:55:06 -0400

    chore: inicializa projeto Spring Boot

---
+++ backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/FoodflowApiApplication.java
@@ -0,0 +1,13 @@
+package dev.pedro.foodflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodflowApiApplication.class, args);
+	}
+
+}
---
+++ backend/foodflow-api/src/test/java/dev/pedro/foodflow_api/FoodflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pedro.foodflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
