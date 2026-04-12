5eb749f457cb8a685dfac9ac722de67e8a941643
Pedro Lucas <plfm3k@gmail.com>
2026-04-12 11:29:01 -0400

    feat: adiciona entidade OrderItem

---
+++ backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/entities/OrderItem.java
@@ -0,0 +1,33 @@
+package dev.pedro.foodflow_api.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal unitPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    public BigDecimal getTotal() {
+        return unitPrice.multiply(BigDecimal.valueOf(quantity));
+    }
+
+}
