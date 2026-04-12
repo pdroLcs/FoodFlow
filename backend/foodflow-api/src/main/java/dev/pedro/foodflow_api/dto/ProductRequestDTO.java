b377b9799ca6c0355685edddc56eeabf3a50b961
Pedro Lucas <plfm3k@gmail.com>
2026-04-12 14:02:32 -0400

    feat: adiciona ProductRequestDTO

---
+++ backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/dto/ProductRequestDTO.java
@@ -0,0 +1,15 @@
+package dev.pedro.foodflow_api.dto;
+
+import dev.pedro.foodflow_api.entities.Category;
+
+import java.math.BigDecimal;
+
+public record ProductRequestDTO(
+        String name,
+        String description,
+        BigDecimal price,
+        String imageUrl,
+        boolean active,
+        Category category
+) {
+}
