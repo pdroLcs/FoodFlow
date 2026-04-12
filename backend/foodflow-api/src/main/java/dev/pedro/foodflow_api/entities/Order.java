a319aa18decd022d61c1df34b7fefb199000d5e7
Pedro Lucas <plfm3k@gmail.com>
2026-04-12 11:38:48 -0400

    feat: adiciona entidade Order

---
+++ backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/entities/Order.java
@@ -0,0 +1,59 @@
+package dev.pedro.foodflow_api.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static dev.pedro.foodflow_api.entities.OrderStatus.PENDING;
+
+@Entity
+@Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Builder.Default
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status = PENDING;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime creationDate;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private BigDecimal totalValue = BigDecimal.ZERO;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items = new ArrayList<>();
+
+    @PrePersist
+    public void prePersist() {
+        this.creationDate = LocalDateTime.now();
+    }
+
+    public void addItem(OrderItem item) {
+        item.setOrder(this);
+        items.add(item);
+    }
+
+    public void calculateTotal() {
+        totalValue = items.stream()
+                .map(item -> item.getUnitPrice()
+                        .multiply(BigDecimal.valueOf(item.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+}
