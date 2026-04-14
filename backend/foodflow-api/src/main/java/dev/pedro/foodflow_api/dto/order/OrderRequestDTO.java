ecb6cc3440b9a2fe37de00660f753bceab86b6cb
Pedro Lucas <plfm3k@gmail.com>
2026-04-14 11:11:36 -0400

    feat: adiciona campo tableId

--- backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/dto/order/OrderRequestDTO.java
+++ backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/dto/order/OrderRequestDTO.java
@@ -2,5 +2,5 @@ package dev.pedro.foodflow_api.dto.order;
 
 import java.util.List;
 
-public record OrderRequestDTO(List<OrderItemRequestDTO> items) {
+public record OrderRequestDTO(Long tableId, List<OrderItemRequestDTO> items) {
 }
