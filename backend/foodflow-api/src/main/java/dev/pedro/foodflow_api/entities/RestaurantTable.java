137ac598b650fad448f43b8d5041609b88946771
Pedro Lucas <plfm3k@gmail.com>
2026-04-14 09:17:34 -0400

    feat: remove entidade User e adiciona entidade RestaurantTable

---
+++ backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/entities/RestaurantTable.java
@@ -0,0 +1,32 @@
+package dev.pedro.foodflow_api.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tables")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class RestaurantTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer number;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "table")
+    private List<Order> orders = new ArrayList<>();
+
+}
--- backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/entities/Role.java
+++
@@ -1,6 +0,0 @@
-package dev.pedro.foodflow_api.entities;
-
-public enum Role {
-    USER,
-    ADMIN
-}
