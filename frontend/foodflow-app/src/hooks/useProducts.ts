596190fe25c5399f067be0d304efc2ff0103f53f
Pedro Lucas <plfm3k@gmail.com>
2026-05-05 21:46:26 -0400

    feat: adiciona dropdown para filtro por categoria no menu do cliente

--- frontend/foodflow-app/src/hooks/useProducts.ts
+++ frontend/foodflow-app/src/hooks/useProducts.ts
@@ -5,7 +5,7 @@ import { api } from "../services/api";
 
 export const useProducts = (isAdmin?: boolean, categoryId?: number) => {
   return useQuery<Product[]>({
-    queryKey: ["products", categoryId],
+    queryKey: ["products", isAdmin, categoryId],
     queryFn: async () => {
       const res = isAdmin ? await fetchWithAuth(api.getProductsAdmin(categoryId)) : await fetch(api.getProducts(categoryId))
       return res.json();
--- frontend/foodflow-app/src/pages/public/Menu.tsx
+++ frontend/foodflow-app/src/pages/public/Menu.tsx
@@ -1,22 +1,30 @@
 import { ProductCard } from "../../components/ProductCard";
 import { useParams } from "react-router-dom";
 import { useProducts } from "../../hooks/useProducts";
-import { useCategories } from "../../hooks/useCategories";
+import { useState } from "react";
+import { CategoryDropdown } from "../../components/CategoryDropdown";
 
 export const Menu = () => {
 
-  const { data: products, isLoading: loadingProducts } = useProducts()
-  const { data: categories, isLoading: loadingCategories } = useCategories()
+  const [selectedCategory, setSelectedCategory] = useState<number | undefined>()
+
+  const { data: products, isLoading: loadingProducts } = useProducts(false, selectedCategory)
 
   const { publicId } = useParams();
 
-  if (loadingCategories || loadingProducts) {
+  const handleSelectCategory = (categoryId: number | undefined) => {
+    setSelectedCategory(categoryId)
+  }
+
+  if (loadingProducts) {
     return <p>Carregando...</p>;
   }
 
   return (
     <div>
       <h1>Menu</h1>
+
+      <CategoryDropdown onSelectCategory={handleSelectCategory}/>
 
       {products?.map(product => (
         <ProductCard key={product.id} product={product}/>
