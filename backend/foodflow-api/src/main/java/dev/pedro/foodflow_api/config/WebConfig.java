0ce432daf52792680372f7fda6e977b05ce775ac
Pedro Lucas <plfm3k@gmail.com>
2026-05-10 16:37:48 -0400

    fix: corrige exibição de imagens estáticas

--- backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/config/WebConfig.java
+++ backend/foodflow-api/src/main/java/dev/pedro/foodflow_api/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**").addResourceLocations("file:uploads/");
+        registry.addResourceHandler("/uploads/**").addResourceLocations("file:/app/uploads/");
         registry.addResourceHandler("/qrcodes/**").addResourceLocations("file:/app/qrcodes/");
     }
 }
