5dd9f192004da2015f6a97463033767d6e98d81d
Pedro Lucas <plfm3k@gmail.com>
2026-05-10 14:26:47 -0400

    fix: corrige importação do useRouter

--- frontend/src/app/login/LoginClient.tsx
+++ frontend/src/app/login/LoginClient.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { login } from "@/src/services/api";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function LoginClient() {
